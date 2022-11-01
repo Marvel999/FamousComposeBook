5d349d6ffa67c2eaf4e2a12374559a18081f2c56
Sparrow007 <ankit.steven007@gmail.com>
2022-11-01 18:37:55 +0530

    adding the topAppBar for instagram

--- app/src/main/java/com/marvel999/fuitemplet/DemoScreen/ExpScreen.kt
+++ app/src/main/java/com/marvel999/fuitemplet/DemoScreen/ExpScreen.kt
@@ -17,7 +17,9 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.marvel999.fuitemplet.DemoScreen.ui.theme.FUITempletTheme
 import com.marvel999.fuitemplet.R
@@ -45,45 +47,45 @@ fun HomeUI() {
 
 }
 
-
+/**
+ * Instagram TopAppBar UI for showing different UI Cases here
+ */
 @Composable
 fun TopAppBar() {
-    Row (
+    Row(
         modifier = Modifier
             .fillMaxWidth()
-            .wrapContentHeight(align = Alignment.Top),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.Top
+            .wrapContentHeight()
     )
     {
-        TopBarCenterImage(res = R.drawable.ic_baseline_camera_alt, Alignment.CenterStart)
-        InstagramText()
-        TopBarCenterImage(res = R.drawable.ic_baseline_send_24, Alignment.CenterEnd)
+        TopBarCenterImage(res = R.drawable.ic_baseline_camera_alt, Modifier.padding(start = 10.dp))
+        Box(
+            modifier = Modifier
+                .wrapContentSize(Alignment.Center)
+                .weight(1f)
+        ) {
+            Text(
+                text = "Instagram",
+                style = TextStyle(
+                    fontFamily = FontFamily.Cursive,
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.ExtraBold,
+                    fontStyle = FontStyle.Italic
+                ),
+                textAlign = TextAlign.Center
+            )
+        }
+        TopBarCenterImage(res = R.drawable.ic_baseline_send_24, Modifier.padding(end = 10.dp))
     }
 
 }
 
 @Composable
-fun RowScope.TopBarCenterImage(@DrawableRes res: Int, alignment: Alignment) {
+fun RowScope.TopBarCenterImage(@DrawableRes res: Int, modifier: Modifier) {
     Image(
         painter = painterResource(id = res),
         contentDescription = null,
-        modifier = Modifier.align(Alignment.CenterVertically),
-        alignment = alignment
-    )
-}
-
-
-@Composable
-fun InstagramText() {
-    Text(
-        text = "Instagram",
-        style = TextStyle(
-            fontFamily = FontFamily.Cursive,
-            fontSize = 32.sp,
-            fontWeight = FontWeight.ExtraBold,
-            fontStyle = FontStyle.Italic
-        )
+        modifier = modifier.align(Alignment.CenterVertically)
     )
 }
 
