4ca31f933819cda402603003164207dbdd99dc76
Sparrow007 <ankit.steven007@gmail.com>
2022-11-13 00:42:59 +0530

    remove extra codes

--- app/src/main/java/com/marvel999/fuitemplet/instagram/InstagramHomeActivity.kt
+++ app/src/main/java/com/marvel999/fuitemplet/instagram/InstagramHomeActivity.kt
@@ -27,25 +27,3 @@ class InstagramHomeActivity : AppCompatActivity() {
         }
     }
 }
-
-fun Activity.makeStatusBarTransparent() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-        window.apply {
-            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                decorView.systemUiVisibility =
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-            } else {
-                decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-            }
-            statusBarColor = Color.TRANSPARENT
-        }
-    }
-}
-
-fun View.setMarginTop(marginTop: Int) {
-    val menuLayoutParams = this.layoutParams as ViewGroup.MarginLayoutParams
-    menuLayoutParams.setMargins(0, marginTop, 0, 0)
-    this.layoutParams = menuLayoutParams
-}
