78bd7c31b9dd6bf428570575da0f5c3d146077d6
Sparrow007 <ankit.steven007@gmail.com>
2022-11-27 04:17:22 +0530

    adding the method for indicators

--- app/src/main/java/com/marvel999/fuitemplet/revampinstagram/RevampInstagramScreen.kt
+++ app/src/main/java/com/marvel999/fuitemplet/revampinstagram/RevampInstagramScreen.kt
@@ -3,6 +3,7 @@ package com.marvel999.fuitemplet.revampinstagram
 import androidx.annotation.DrawableRes
 import androidx.compose.animation.*
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -12,15 +13,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.VerticalPager
-import com.google.accompanist.pager.rememberPagerState
+import com.google.accompanist.pager.*
 import com.marvel999.fuitemplet.R
 import com.marvel999.fuitemplet.ui.theme.FUITempletTheme
 
@@ -54,6 +54,41 @@ fun ShowViewPager() {
 
     }
 
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun ImageViewPager() {
+    val list = listOf(
+        R.drawable.neon_big,
+        R.drawable.neon_man,
+        R.drawable.neon_man,
+        R.drawable.neon_man,
+        R.drawable.neon_man,
+    )
+    Column (Modifier.fillMaxSize()) {
+        val pageState = rememberPagerState()
+        HorizontalPager(count = 10,
+        state = pageState
+        ) {
+            Image(painter = painterResource(list[1]),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(400.dp)
+                    .clip(RoundedCornerShape(30.dp)),
+                contentDescription = null,
+                contentScale = ContentScale.FillHeight
+            )
+        }
+        HorizontalPagerIndicator(
+            pagerState = pageState,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.CenterHorizontally)
+                .padding(16.dp),
+        )
+
+    }
 }
 
 @Composable
@@ -100,6 +135,6 @@ fun ShowAnimationOfHeart() {
 @Composable
 fun ShowPreview() {
     FUITempletTheme {
-        ShowBlurBackground()
+        ImageViewPager()
     }
 }
