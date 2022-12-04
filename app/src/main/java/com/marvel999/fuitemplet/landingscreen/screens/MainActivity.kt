0b0d24c255e5d5fa32a3beaef4ba2ecd00c01bce
Manish <88077753+Manish-Marvel@users.noreply.github.com>
2022-12-04 16:28:45 +0530

    Fix navigation issue (#15)

--- app/src/main/java/com/marvel999/fuitemplet/landingscreen/screens/MainActivity.kt
+++ app/src/main/java/com/marvel999/fuitemplet/landingscreen/screens/MainActivity.kt
@@ -13,14 +13,18 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import com.marvel999.fuitemplet.DemoScreen.DemoScreen
 import com.marvel999.fuitemplet.R
 import com.marvel999.fuitemplet.Spotify.SpotifyActivity
 import com.marvel999.fuitemplet.Spotify.ui.theme.SpotifyTheme
+import com.marvel999.fuitemplet.instagram.InstagramHomeActivity
 import com.marvel999.fuitemplet.landingscreen.componets.LandingPageListItemCard
 import com.marvel999.fuitemplet.landingscreen.componets.LandingPageListItemCardData
 import com.marvel999.fuitemplet.landingscreen.data.landingPageListItemCardDataList
@@ -40,11 +44,12 @@ class MainActivity : ComponentActivity() {
     }
 
     fun StartDemoActivity() {
-        startActivity(Intent(this, DemoScreen::class.java))
+        startActivity(Intent(this@MainActivity, DemoScreen::class.java))
     }
 
     @Composable
-    fun HomeCompose(scaffoldState: ScaffoldState) {
+    fun HomeCompose(scaffoldState: ScaffoldState ) {
+
         Scaffold(
             scaffoldState = scaffoldState,
             topBar = { MyTopAppBar() },
@@ -73,16 +78,24 @@ class MainActivity : ComponentActivity() {
         landingPageListItemCardDataList =
             landingPageListItemCardDataList + LandingPageListItemCardData(
                 appIcon = R.drawable.ic_hacktober_icon,
-                appTemplateName = "Welcome Screen",
+                appTemplateName = stringResource(id = R.string.title_activity_demo_screen),
                 onItemClick = {
                     StartDemoActivity()
                 })
         landingPageListItemCardDataList =
             landingPageListItemCardDataList + LandingPageListItemCardData(
                 appIcon = R.drawable.ic_spotify_logo,
-                appTemplateName = "Spotify",
+                appTemplateName = stringResource(id = R.string.title_spotify_screen),
                 onItemClick = {
-                   startActivity(Intent(this@MainActivity, SpotifyActivity::class.java))
+                    startActivity(Intent(this@MainActivity, SpotifyActivity::class.java))
+                })
+
+        landingPageListItemCardDataList =
+            landingPageListItemCardDataList + LandingPageListItemCardData(
+                appIcon = R.drawable.ic_instagram_logo,
+                appTemplateName = stringResource(id = R.string.title_instagram_screen),
+                onItemClick = {
+                    startActivity(Intent(this@MainActivity, InstagramHomeActivity::class.java))
                 })
         Box(
             modifier = Modifier
@@ -93,17 +106,20 @@ class MainActivity : ComponentActivity() {
             Column(
                 modifier = Modifier.fillMaxSize(),
             ) {
-                TopListSection(landingPageListItemCardDataList)
+                TopListSection()
 
             }
         }
     }
 
     @Composable
-    fun TopListSection(itemList: List<LandingPageListItemCardData>) {
+    fun TopListSection() {
+        val mutableList = remember {
+            mutableStateOf(landingPageListItemCardDataList)
+        }
         Column {
             LazyColumn {
-                itemsIndexed(itemList) { _, item ->
+                itemsIndexed(mutableList.value) { _, item ->
                     LandingPageListItemCard(landingPageListItemCardData = item)
                 }
             }
